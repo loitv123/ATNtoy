d3b70705410c4ae7bf14970ba28950ea05d7ec14
loitv123 <106915274+loitv123@users.noreply.github.com>
2023-06-15 17:17:08 +0700

    Decrese size

--- Page/index.php
+++ Page/index.php
@@ -9,13 +9,7 @@ include 'header3.php';
   <div id="carouselExampleSlidesOnly" class="carousel slide" data-mdb-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="../image/banner1.png" class="d-block w-100" alt="Wild Landscape"/>
-    </div>
-    <div class="carousel-item">
-      <img src="../image/banner1.jpg" class="d-block w-100" alt="Camera"/>
-    </div>
-    <div class="carousel-item">
-      <img src="../image/banner1.jpg" class="d-block w-100" alt="Exotic Fruits"/>
+      <img src="../image/banner.jpg " class="d-block w-100" alt="Wild Landscape"/>
     </div>
   </div>
 </div>
